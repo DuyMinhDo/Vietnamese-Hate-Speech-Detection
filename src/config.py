a860a23bffb5e3b0fbeadea0644efad63cf72f74
QTVxDevil <quyettv1302@gmail.com>
2025-03-27 00:08:24 +0700

    final setup

--- src/config.py
+++ src/config.py
@@ -4,21 +4,28 @@ import torch
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(BASE_DIR, "..", "dataset", "processed") 
 MODEL_DIR = os.path.join(BASE_DIR, "..", "trained_models")  
+LOG_DIR = os.path.join(BASE_DIR, "..", "logs")
+DEMO_DATA_DIR = os.path.join(BASE_DIR, "..", "dataset", "demo_data")
 
 TRAIN_FILE = os.path.join(DATA_DIR, "processed_train.csv")
 DEV_FILE = os.path.join(DATA_DIR, "processed_dev.csv")
 TEST_FILE = os.path.join(DATA_DIR, "processed_test.csv")
 MODEL_SAVE_PATH = os.path.join(MODEL_DIR, "phobert_model")
+LOG_SAVE_PATH = os.path.join(LOG_DIR, "phobert_model")
+TEST_JSON_FILE = os.path.join(DEMO_DATA_DIR, "NLP_test.json")
 
 MODEL_NAME = "vinai/phobert-base"
-MAX_LENGTH = 50
-BATCH_SIZE = 32 
+MAX_LENGTH = 70
+BATCH_SIZE = 16
 EPOCHS = 10
+WARMUP_RATIO = 0.1
+WEIGHT_DECAY = 0.01
+LOGGING_STEPS = 10
 LEARNING_RATE = 2e-5
-WARMUP_RATIO = 0.1 
-WEIGHT_DECAY = 0.01  
-FP16 = True 
+DROPOUT = 0.3
+PATIENCE = 2
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
 os.makedirs(MODEL_SAVE_PATH, exist_ok=True)
+os.makedirs(LOG_SAVE_PATH, exist_ok=True)
--- src/model.py
+++ src/model.py
@@ -1,16 +1,33 @@
 import torch
 import torch.nn as nn
 from transformers import AutoModel
-from src.config import MODEL_NAME
+from src.config import MODEL_NAME, DROPOUT, PATIENCE
 
 class PhoBERTClassifier(nn.Module):
     def __init__(self):
         super(PhoBERTClassifier, self).__init__()
         self.phobert = AutoModel.from_pretrained(MODEL_NAME)
-        self.dropout = nn.Dropout(0.3)
+        self.dropout = nn.Dropout(DROPOUT)
         self.fc = nn.Linear(self.phobert.config.hidden_size, 3)
 
     def forward(self, input_ids, attention_mask):
         outputs = self.phobert(input_ids=input_ids, attention_mask=attention_mask)
         pooled_output = outputs.last_hidden_state[:, 0, :]
         return self.fc(self.dropout(pooled_output))
+
+class EarlyStopping:
+    def __init__(self, patience=PATIENCE, delta=0):
+        self.patience = patience
+        self.delta = delta
+        self.best_loss = float("inf")
+        self.counter = 0
+        self.early_stop = False
+
+    def __call__(self, val_loss):
+        if val_loss < self.best_loss - self.delta:
+            self.best_loss = val_loss
+            self.counter = 0
+        else:
+            self.counter += 1
+            if self.counter >= self.patience:
+                self.early_stop = True
